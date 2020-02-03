b8433e7330df51685a080e8de49752f3992c139d
ada popovic <popowucz.adrianna@gmail.com>
2020-02-03 16:39:54 +0100

    starting build-up

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+window.onload=function(){
+  const el = document.querySelector("#module");
+
+  el.addEventListener("mousemove", (e) => {
+    el.style.backgroundPositionX = -e.offsetX + "px";
+    el.style.backgroundPositionY = -e.offsetY + "px";
+  });
+}
