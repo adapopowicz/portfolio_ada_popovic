3d9a6186c3a9b3b316f11cccb45dc43c83e098f1
ada popovic <popowucz.adrianna@gmail.com>
2020-02-08 18:57:03 +0100

    --

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-
-
 window.onload=function(){
+
+  // const image1 = document.querySelector("#module");
+
   const el = document.querySelector("#module");
+  // const startingPositionBart = document.querySelector("#bart").style.left;
 
   el.addEventListener("mousemove", (e) => {
     el.style.backgroundPositionX = -e.offsetX + "px";
     el.style.backgroundPositionY = -e.offsetY + "px";
+
+    document.querySelector("#bart > img").style.left = 1000-e.offsetX + "px";
+    document.querySelector("#bart > img").style.top = 550-e.offsetY + "px";
+    document.querySelector("#flyer > img").style.left = 1600-e.offsetX + "px";
+    document.querySelector("#flyer > img").style.top = 850-e.offsetY + "px";
+    document.querySelector("#liver > img").style.left = 1500-e.offsetX + "px";
+    document.querySelector("#liver > img").style.top = 750-e.offsetY + "px";
+    document.querySelector("#pizza > img").style.left = 900-e.offsetX + "px";
+    document.querySelector("#pizza > img").style.top = 750-e.offsetY + "px";
   });
+  // $('#bart').css('margin-left', "50"+e.offsetX+"%");
+
 }
